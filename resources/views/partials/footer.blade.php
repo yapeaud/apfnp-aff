846ab246a9b6fcbccd304f7cd6dae1b436075243
Yapeaud <yapoabed@gmail.com>
2024-03-11 18:00:41 +0100

    apfnp

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,66 @@
+<footer class="Footer" style="background-image: url('assets/img/illustrations/BOTTOM.png')">
+    <div class="pb-x1 px-3 px-lg-0">
+        <div class="container">
+            <div class="row align-items-end g-4 g-sm-6">
+                <div class="col-6 col-md-4">
+                    <div class="mb-6 mb-md-8"><a class="cursor-pointer" href=""><img class="img-fluid"
+                                src="assets/img/logos/Footer_logo.png" alt="" /></a></div>
+                    <div>
+                        <div class="mb-2"> <a class="links" href="#">home</a></div>
+                        <div class="mb-2"> <a class="links" href="#about">about</a></div>
+                        <div class="mb-2"> <a class="links" href="#products">catalog</a></div>
+                        <div class="mb-2"> <a class="links" href="#review">
+                                review
+                            </a></div><a class="links" href="#support">
+                            support
+                        </a>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <div class="mb-3 mb-md-5">
+                        <h3 class="fs-9 fs-md-8 fw-bold mb-0" style="color: #FFF0D3;">User Account</h3>
+                    </div>
+                    <div>
+                        <div class="mb-2"> <a class="links" href="#!">sign up</a></div>
+                        <div class="mb-2"> <a class="links" href="#!">log in </a></div>
+                        <div class="mb-2"> <a class="links" href="#!">edit profile</a></div>
+                        <div class="mb-2"> <a class="links" href="#!">
+                                history
+                            </a></div><a class="links" href="#!">
+                            refund
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="row g-4 g-sm-6 g-md-0">
+                        <div class="col-6 col-md-12 mb-md-7">
+                            <h3 class="fs-9 fs-md-8 fw-bold mb-3" style="color: #FFF0D3;">Follow Us At</h3>
+                            <div class="d-flex align-items-center"> <a class="social-icon me-2" href="#!"><span
+                                        class="uil uil-facebook fs-8"></span></a><a class="social-icon me-2"
+                                    href="#!"><span class="uil uil-linkedin fs-8"></span></a><a
+                                    class="social-icon me-2" href="#!"><span
+                                        class="uil uil-youtube fs-8"></span></a><a class="social-icon"
+                                    href="#!"><span class="uil uil-twitter fs-8"></span></a></div>
+                        </div>
+                        <div class="col-6 col-md-12">
+                            <div class="mb-3">
+                                <p class="fs-9 fs-md-8 fw-bold lh-nomal mb-0" style="color: #FFF0D3;"> Contact </p>
+                            </div>
+                            <div class="fs-10 fs-md-9">
+                                <p class="mb-0 text-white opacity-70"> Obito Uchiha</p>
+                                <p class="mb-0 text-white opacity-70"> Konoha, Leaf Village</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-success py-2 py-md-3 position-relative terms-condition">
+        <div class="overley-background"></div>
+        <div class="container py-12 text-div text-md-end"><a class="links ms-md-4" href="#!" title="F.A.Q"> F.A.Q
+            </a><a class="links ms-3 ms-md-4" href="#!" title="Cookies Policy"> Cookies Policy</a><a
+                class="links ms-3 ms-md-4" href="#!" title="Legal Terms"> Legal Terms </a><a
+                class="links ms-3 ms-md-4" href="#!" title="Privacy Policy"> Privacy Policy</a></div>
+    </div>
+</footer>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg py-1" id="navbar-top" data-navbar-soft-on-scroll="data-navbar-soft-on-scroll">
+    <div class="container"><a class="navbar-brand me-lg-auto cursor-pointer" href=""><img class="w-50 w-md-100 img-fluid" src="assets/img/logos/logo.png" alt="" /></a>
+        <button class="navbar-toggler border-0 pe-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" data-navbar-collapse="data-navbar-collapse">
+            <div class="container d-lg-flex justify-content-lg-end pe-lg-0 w-lg-100">
+                <form class="form-inline position-relative w-lg-50 ms-lg-4 ms-xl-9 mt-3 mt-lg-0" onsubmit="return false;">
+                    <input class="search fs-8 bg-transparent form-control" type="search" name="search" placeholder="search plants..." />
+                    <div class="search-icon"> <span class="uil uil-search"></span></div>
+                </form>
+                <ul class="navbar-nav mt-2 mt-lg-1 ms-lg-4 ms-xl-8 ms-2xl-9 gap-lg-x1" data-navbar-nav="data-navbar-nav">
+                    <li class="nav-item"> <a class="nav-link nav-bar-item px-0" href="#home" title="home">home</a>
+                    </li>
+                    <li class="nav-item"> <a class="nav-link nav-bar-item px-0" href="#about" title="about">about</a>
+                    </li>
+                    <li class="nav-item"> <a class="nav-link nav-bar-item px-0" href="#products" title="catalog">catalog</a></li>
+                    <li class="nav-item"> <a class="nav-link nav-bar-item px-0" href="#review" title="reviews">reviews</a></li>
+                    <li class="nav-item"> <a class="nav-link nav-bar-item px-0" href="#support" title="support">support</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
