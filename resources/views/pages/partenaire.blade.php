14037bece276d5b976b740977c7f41f35a805a9a
Yapeaud <yapoabed@gmail.com>
2024-04-15 18:58:59 +0200

    apfnp

--- resources/views/pages/partenaire.blade.php
+++ resources/views/pages/partenaire.blade.php
@@ -10,67 +10,74 @@
         <section>
             <p class="text-secondary text-capitalize fw-light mb-0 fs-5 fs-lg-4 text-center mb-7 mb-lg-8">
               nos<span class="fw-bold"> partenaires </span></p>
+
             <div class="row mb-13 w-lg-75 mb-lg-5 mx-auto gy-2 gy-lg-5 h-100">
               <article class="col-12 col-md-6 col-lg-4 px-3 text-center mb-5 mb-lg-0">
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
                     src="assets/img/partenaires/nitidae.jfif" alt="" />
-                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook"></span></a><a href="#!">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!"> <span
-                        class="uil uil-linkedin"></span></a></div>
+                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook" hidden></span></a><a href="#!">
+                      <span class="uil uil-instagram-alt" hidden></span></a><a href="#!"> <span
+                        class="uil uil-linkedin" hidden></span></a></div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">NITIDAE</h3>
                 <h5 class="text-black fs-10 fs-md-9 fw-medium text-capitalize mb-3">changement climatique</h5>
               </article>
+
               <article class="col-12 col-md-6 col-lg-4 px-3 text-center mb-5 mb-lg-0">
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
                     src="assets/img/partenaires/wcf.jpg" alt="" />
-                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook"></span></a><a href="#!">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!"> <span
-                        class="uil uil-linkedin"></span></a></div>
+                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook" hidden></span></a><a href="#!">
+                      <span class="uil uil-instagram-alt" hidden></span></a><a href="#!"> <span
+                        class="uil uil-linkedin" hidden></span></a></div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">WCF</h3>
                 <h5 class="text-black fs-10 fs-md-9 fw-medium text-capitalize mb-3">gestion durable des forêts, observation independante</h5>
-                
               </article>
+
               <article class="col-12 col-md-6 col-lg-4 px-3 text-center mb-5 mb-lg-0">
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
                     src="assets/img/partenaires/ClientEarth.png" alt="" />
-                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook"></span></a><a href="#!">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!"> <span
-                        class="uil uil-linkedin"></span></a></div>
+                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook" hidden></span></a><a href="#!">
+                      <span class="uil uil-instagram-alt" hidden></span></a><a href="#!"> <span
+                        class="uil uil-linkedin" hidden></span></a></div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">CLIENTEARTH</h3>
                 <h5 class="text-black fs-10 fs-md-9 fw-medium text-capitalize mb-3">analyse juridique et gouvernance forestière</h5>
-                
               </article>
+
               <article class="col-12 col-md-6 col-lg-4 px-3 text-center mb-5 mb-lg-0">
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
                     src="assets/img/partenaires/redd.png" alt="" />
-                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook"></span></a><a href="#!">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!"> <span
-                        class="uil uil-linkedin"></span></a></div>
+                  <div class="social-icons"><a href="#!"><span class="uil uil-facebook" hidden></span></a>
+                    <a href="#!"><span class="uil uil-instagram-alt" hidden></span></a>
+                    <a href="#!"><span class="uil uil-linkedin" hidden></span></a></div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">MECANISME REDD+</h3>
                 <h5 class="text-black fs-10 fs-md-9 fw-medium text-capitalize mb-3">changement climatique</h5>
-                
               </article>
+
               <article class="col-12 col-md-6 col-lg-4 px-3 text-center mb-5 mb-lg-0">
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
                     src="assets/img/partenaires/apv-flegt.jpg" alt="" />
-                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook"></span></a><a href="#!">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!">
-                      <span class="uil uil-linkedin"></span></a></div>
+                  <div class="social-icons">
+                    <a href="#!" hidden><span class="uil uil-facebook"></span></a>
+                    <a href="#!" hidden><span class="uil uil-instagram-alt"></span></a>
+                    <a href="#!"><span class="uil uil-linkedin"hidden></span></a></div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">MECANISME FLEGT</h3>
                 <h5 class="text-black fs-10 fs-md-9 fw-medium text-capitalize mb-3">gouvernance forestière</h5>
-                
               </article>
+
               <article class="col-12 col-md-6 col-lg-4 px-3 text-center mb-5 mb-lg-0">
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
-                    src="assets/img/partenaires/" alt="" />
-                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook"></span></a><a href="#!">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!">
-                      <span class="uil uil-linkedin"></span></a></div>
+                    src="assets/img/partenaires/oici.png" alt="" />
+                  <div class="social-icons"><a href="#!"> 
+                    <span class="uil uil-facebook" hidden>
+                    </span></a>
+                    <a href="#!">
+                      <span class="uil uil-instagram-alt" hidden></span></a><a href="#!">
+                      <span class="uil uil-linkedin" hidden></span></a>
+                    </div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">Réseau-OICI</h3>
                 <h5 class="text-black fs-10 fs-md-9 fw-medium text-capitalize mb-3">pour la loi et les communautés</h5>
@@ -79,9 +86,9 @@
               <article class="col-12 col-md-6 col-lg-4 px-3 text-center mb-5 mb-lg-0">
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
                     src="assets/img/partenaires/" alt="" />
-                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook"></span></a><a href="#!">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!">
-                      <span class="uil uil-linkedin"></span></a></div>
+                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook" hidden></span></a><a href="#!">
+                      <span class="uil uil-instagram-alt" hidden></span></a><a href="#!">
+                      <span class="uil uil-linkedin" hidden></span></a></div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">DEVRS</h3>
                 <h5 class="text-black fs-10 fs-md-9 fw-medium text-capitalize mb-3">economie verte</h5>
@@ -91,9 +98,9 @@
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
                     src="assets/img/partenaires/cga.jpg" alt="" />
                   <div class="social-icons"><a href="#!"> 
-                    <span class="uil uil-facebook"></span></a><a href="https://web.facebook.com/SCOOPAGEACOOPCA">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!">
-                      <span class="uil uil-linkedin"></span></a>
+                    <span class="uil uil-facebook" hidden></span></a><a href="https://web.facebook.com/SCOOPAGEACOOPCA">
+                      <span class="uil uil-instagram-alt" hidden></span></a><a href="#!">
+                      <span class="uil uil-linkedin" hidden></span></a>
                     </div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">Coopérative Agricole Gédéon d’Afféry</h3>
@@ -103,9 +110,9 @@
               <article class="col-12 col-md-6 col-lg-4 px-3 text-center mb-5 mb-lg-0">
                 <div class="position-relative w-75 w-md-100 mx-auto mb-2"><img class="w-100 team-hero-image"
                     src="assets/img/partenaires/logo-commune-affery.png" alt="" />
-                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook"></span></a><a href="#!">
-                      <span class="uil uil-instagram-alt"></span></a><a href="#!">
-                      <span class="uil uil-linkedin"></span></a></div>
+                  <div class="social-icons"><a href="#!"> <span class="uil uil-facebook" hidden></span></a><a href="#!">
+                      <span class="uil uil-instagram-alt" hidden></span></a><a href="#!">
+                      <span class="uil uil-linkedin" hidden></span></a></div>
                 </div>
                 <h3 class="fs-9 fs-md-8 text-success fw-bold mb-1">Mairie d’Afféry</h3>
                 <h5 class="text-black fs-10 fs-md-9 fw-medium text-capitalize mb-3"></h5>
